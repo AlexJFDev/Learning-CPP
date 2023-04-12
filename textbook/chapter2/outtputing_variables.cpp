82d4b165b4694be0d75a46116c7f99ef09fe6d9f
AlexJFDev <alexjfuhr@gmail.com>
2023-04-12 12:55:27 -0400

    Completed chapter 2 exercises

---
+++ textbook/chapter2/outtputing_variables.cpp
@@ -0,0 +1,21 @@
+// 2.12 Exercise 1
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int hour, minute;
+    char colon;
+
+    hour = 11;
+    minute = 59;
+    colon = ':';
+
+    cout << "The current time is ";
+    cout << hour;
+    cout << colon;
+    cout << minute;
+    cout << endl;
+    
+    return 0;
+}
