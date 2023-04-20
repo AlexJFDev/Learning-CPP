355f55c497abe7f8d9c09e81e4ed5cb2fc336a24
AlexJFDev <alexjfuhr@gmail.com>
2023-04-20 14:07:38 -0400

    Forgot to commit for a while

--- textbook/chapter3/3.12.1.cpp
+++ textbook/chapter3/3.12.1.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main()
 {
     cout << "First Line." << endl;
-    newLine();
+    new_line();
     cout << "Second Line." << endl;
     return 0;
 }
---
+++ textbook/chapter8/struts.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+using namespace std;
+
+void print(string str, string end = "\n") {
+    cout << str << end;
+}
+
+void print(double dub, string end = "\n"){
+    cout << dub << endl;
+}
+
+struct Point {
+    double x, y;
+
+    string as_string() {
+        return "Point(" + to_string(this->x) + ", " + to_string(this->y) + ")";
+    }
+
+    double euclidean_distance_to(Point point) {
+        double dx = point.x - this->x;
+        double dy = point.y - this->y;
+        return sqrt(dx*dx + dy*dy);
+    }
+};
+
+struct Rectangle{
+    Point upper_left_corner;
+    double width, height;
+
+    Point get_lower_right() {
+        Point temp = {this->upper_left_corner.x + width, this->upper_left_corner.y - height};
+        return temp;
+    }
+
+    double get_area() {
+        return width * height;
+    }
+};
+
+int main() {
+    Point origin = {0, 0};
+    Point other_point = {3.65, 66.2};
+
+    Rectangle rect = {origin, 5, 5};
+
+    print(rect.get_lower_right().as_string());
+    print(rect.get_area());
+}
+
